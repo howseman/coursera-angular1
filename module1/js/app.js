f7c7eca34b3d7ee14849505e96bb2166d8daae56
Howseman Leal <howsmayer@hotmail.com>
2017-02-25 12:51:56 -0500

    module 1 assignment ready

--- module1/js/app.js
+++ module1/js/app.js
@@ -6,6 +6,37 @@
 
   LunchCheckController.$inject = ['$scope'];
   function LunchCheckController ($scope) {
+    $scope.lunchItems = "";
+    $scope.userMessage = "";
+    $scope.messageType = "";
 
+    $scope.checkQty = function() {
+      //var re = /\s*,\s*/; //RegExpr to clean spaces before and after each comma
+                            //Useful if we use the content string
+                            //var lunchItems = $scope.lunchItems.split(re);
+      var lunchItems = $scope.lunchItems.split(',');
+      var itemsCounter = 0;
+
+      for (var i = 0; i < lunchItems.length; i++) {
+        if (lunchItems[i] != "") {
+          itemsCounter++;
+        }
+      }
+
+      // console.log(lunchItems.length + ' items in list');
+      // console.log(lunchItems);
+      console.log(itemsCounter + ' valid items in list');
+
+      if (itemsCounter > 3) {
+        $scope.userMessage = 'Too much!';
+        $scope.messageType = 'alert-danger';
+      } else if (itemsCounter == 0) {
+        $scope.userMessage = 'Please enter data first';
+        $scope.messageType = 'alert-warning';
+      } else {
+        $scope.userMessage = 'Enjoy!';
+        $scope.messageType = 'alert-success';
+      }
+    };
   };
 })();
